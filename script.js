f144e0c0d89259d46d33cc27d331b4512eeab6e2
KulichO <oaosokina@yandex.ru>
2025-05-19 18:13:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let showButton = document.querySelector('.button--show');
+let textChange = document.querySelector('.next--show');
+let list = document.querySelector('.brands__list');
+
+showButton.addEventListener ('click', function(){
+    showButton.classList.toggle('button--hide');
+    textChange.classList.toggle('next--hide');
+    list.classList.toggle('brands__list--show');
+});
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1.2,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+  })
