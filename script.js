26b5f61b1b2ebf34cad7bf30e10e59a6256d58eb
KulichO <oaosokina@yandex.ru>
2025-05-22 22:05:17 +0300

    change2

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ showButton.addEventListener ('click', function(){
     showButton.classList.toggle('button--hide');
     textChange.classList.toggle('next--hide');
     list.classList.toggle('brands__list--show');
+    
+    if (showButton.classList.contains('button--hide')) {
+        showButton.textContent = 'Скрыть';
+    } else {
+        showButton.textContent = 'Показать все';
+    }
+
 });
 
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 1.2,
-  
+
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
